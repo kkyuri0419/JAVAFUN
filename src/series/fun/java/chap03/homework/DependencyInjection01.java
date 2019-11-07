bb0f554ef35559e05bfd9c2b75d4a442dae3cc48
MinHyungRHIE <quotia72@naver.com>
2019-11-08 00:25:11 +0900

    1107_homework

---
+++ src/series/fun/java/chap03/homework/DependencyInjection01.java
@@ -0,0 +1,19 @@
+package series.fun.java.chap03.homework;
+
+class A{
+	
+}
+
+class B{
+	
+}
+
+class C{
+	
+}
+
+public class DependencyInjection01 {
+	public static void main(String[] args) {
+		
+	}
+}
